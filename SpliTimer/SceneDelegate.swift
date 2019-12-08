ddc0e5c3be51e0a25249227b304c554b5d125f70
Enddle <Enddle@users.noreply.github.com>
2019-12-08 10:38:47 -0500

    SwiftUI demo
    - Convert to swiftui

---
+++ SpliTimer/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  SpliTimer
+//
+//  Created by Enddle Zheng on 12/8/19.
+//  Copyright © 2019 Enddle Zheng. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOpitions: UIScene.ConnectionOptions) {
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: TimerView())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
---
+++ SpliTimer/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  SpliTimer
+//
+//  Created by Enddle Zheng on 12/8/19.
+//  Copyright © 2019 Enddle Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
