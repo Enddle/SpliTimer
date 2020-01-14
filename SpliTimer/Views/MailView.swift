0173d354d3af700b2ab8ea22d9ed023f6968b191
Enddle <Enddle@users.noreply.github.com>
2020-01-14 13:41:54 -0800

    Update Settings
    - added about
    - added send feedback mail
    - fixed toggle cannot save by delay

---
+++ SpliTimer/Views/MailView.swift
@@ -0,0 +1,59 @@
+//
+//  MailView.swift
+//
+//  https://stackoverflow.com/questions/56784722/swiftui-send-email
+//  By Matteo Pacini
+//
+
+import UIKit
+import SwiftUI
+import MessageUI
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Binding var isShowing: Bool
+    @Binding var result: Result<MFMailComposeResult, Error>?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var isShowing: Bool
+        @Binding var result: Result<MFMailComposeResult, Error>?
+
+        init(isShowing: Binding<Bool>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _isShowing = isShowing
+            _result = result
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                isShowing = false
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShowing: $isShowing,
+                           result: $result)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        vc.setToRecipients(["support@enddle.me"])
+        vc.setSubject("Feedback on App \"SpliTimer\"")
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+
+    }
+}
--- SpliTimer/Views/SettingsView.swift
+++ SpliTimer/Views/SettingsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct SettingsView: View {
     
@@ -16,75 +17,132 @@ struct SettingsView: View {
     
     @State var showRemoveAllAlert = false
     
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
+    @State var isShowingMailError = false
+    
     var body: some View {
-        List {
-            Section(){
+        ZStack {
+            NavigationView {
+                List {
+                    Section(header: Text("Timer"), footer: Text("When adding a new timer, the label will be " + (timerVM.useEmptyLabel ? "empty." : timerVM.placeholder + " + id.") )) {
+                        
+                        Stepper("Timer Rows: \(timerVM.rows)", value: $timerVM.rows, in: 1...5, onEditingChanged: { _ in
+                            self.timerVM.saveSettings()
+                        })
+                        Stepper("Timer Columns: \(timerVM.columns)", value: $timerVM.columns, in: 1...5, onEditingChanged: { _ in
+                            self.timerVM.saveSettings()
+                        })
+                        Stepper("Timer List Items: \(timerVM.items)", value: $timerVM.items, in: timerVM.rows*timerVM.columns...25, onEditingChanged: { _ in
+                            self.timerVM.saveSettings()
+                        })
+                        
+                        Toggle("Create Empty Label", isOn: $timerVM.useEmptyLabel)
+                            .onTapGesture {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                    self.timerVM.saveSettings()
+                                }
+                        }
+                        
+                        HStack(alignment: .firstTextBaseline) {
+                            Text("Label Placeholder")
+                            Spacer()
+                            TextField("Text", text: $timerVM.placeholder, onCommit: {
+                                self.timerVM.saveSettings()
+                            })
+                                .multilineTextAlignment(.trailing)
+                        }
+                        .disabled(timerVM.useEmptyLabel)
+                        .foregroundColor(timerVM.useEmptyLabel ? Color(.secondaryLabel) : Color(.label))
+                    }
 
-                Button("Remove All Timers", action: {
-                    self.showRemoveAllAlert = true
-                })
-                .alert(isPresented:$showRemoveAllAlert) {
-                    Alert(title: Text("Remove All Timers"), message: Text("This action cannot be undone"), primaryButton: .destructive(Text("Delete")) {
-                            self.timerVM.removeAllTimers()
-                    }, secondaryButton: .cancel())
+                    Section(){
+
+                        Button("Remove All Timers", action: {
+                            self.showRemoveAllAlert = true
+                        })
+                        .alert(isPresented:$showRemoveAllAlert) {
+                            Alert(title: Text("Remove All Timers"), message: Text("\nThis action cannot be undone"), primaryButton: .destructive(Text("Delete")) {
+                                    self.timerVM.removeAllTimers()
+                            }, secondaryButton: .cancel())
+                        }
+
+        //                Button("Clear Data", action: {
+        //                    UserDefaults.standard.set([], forKey: "SavedLabels")
+        //                })
+                        
+                        Button("Restore Settings", action: {
+                            self.timerVM.restoreSettings()
+                        })
+                    }
+                                
+                    Section(header: Text("Icon")) {
+
+                        Button("Reset Icon", action: {
+                            self.icon.changeAppIcon(.reset)
+                        })
+                        
+                        Button("Change to Grey Icon", action: {
+                            self.icon.changeAppIcon(.greyIcon)
+                        })
+                        
+                        Button("Change to Grey Line Icon", action: {
+                            self.icon.changeAppIcon(.greyLineIcon)
+                        })
+                    }
+                    
+                    Section(header: Text("About")) {
+                        VStack (alignment: .center) {
+                            
+                            Image("icon")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(18)
+                            
+                            Text("SpliTimer")
+                                .font(.headline)
+                                .padding()
+                            
+                            Text("Version: \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "unknown")")
+                                .font(.subheadline)
+                                .padding()
+                            
+                            Text("Developer: Enddle Zheng")
+                                .font(.subheadline)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        
+                        Button("Send Feedback Email", action: {
+                            self.isShowingMailView.toggle()
+                        })
+                    }
                 }
-
-                Button("Clear Data", action: {
-                    UserDefaults.standard.set([], forKey: "SavedLabels")
-                })
+                .listStyle(GroupedListStyle())
+                .padding(.bottom, self.appearance.keyboardSafeInset)
+                .navigationBarTitle("Settings")
             }
             
-            Section() {
-                
-                Stepper("Timer Rows: \(timerVM.rows)", value: $timerVM.rows, in: 1...5, onEditingChanged: { _ in
-                    self.timerVM.saveSettings()
-                })
-                Stepper("Timer Columns: \(timerVM.columns)", value: $timerVM.columns, in: 1...5, onEditingChanged: { _ in
-                    self.timerVM.saveSettings()
-                })
-                Stepper("Timer List Items: \(timerVM.items)", value: $timerVM.items, in: timerVM.rows*timerVM.columns...25, onEditingChanged: { _ in
-                    self.timerVM.saveSettings()
-                })
-                
-                Toggle("Create Empty Label", isOn: $timerVM.useEmptyLabel)
-                    .onTapGesture {
-                        self.timerVM.saveSettings()
-                }
-                
-                HStack(alignment: .firstTextBaseline) {
-                    Text("Placeholder Text")
-                    Spacer()
-                    TextField("Text", text: $timerVM.placeholder, onCommit: {
-                        self.timerVM.saveSettings()
-                    })
-                        .multilineTextAlignment(.trailing)
-                }
-                .disabled(timerVM.useEmptyLabel)
-                .foregroundColor(timerVM.useEmptyLabel ? Color(.secondaryLabel) : Color(.label))
-                
-                Button("Restore Settings", action: {
-                    self.timerVM.restoreSettings()
-                })
+            if (isShowingMailView) {
+                mailView()
+                    .transition(.move(edge: .bottom))
+                    .animation(.default)
             }
-            
-            Section() {
-
-                Button("Reset Icon", action: {
-                    self.icon.changeAppIcon(.reset)
-                })
-                
-                Button("Change to Grey Icon", action: {
-                    self.icon.changeAppIcon(.greyIcon)
-                })
-                
-                Button("Change to Grey Line Icon", action: {
-                    self.icon.changeAppIcon(.greyLineIcon)
-                })
-            }
-            
         }
-        .listStyle(GroupedListStyle())
-        .padding(.bottom, self.appearance.keyboardSafeInset)
+        .alert(isPresented: $isShowingMailError) {
+            Alert(title: Text("Cannot Send Email"), message: Text("\nCan't send emails from this device"))
+        }
+    }
+
+    private func mailView() -> some View {
+        
+        if !MFMailComposeViewController.canSendMail() {
+            
+            self.isShowingMailError = true
+            return AnyView(EmptyView())
+        }
+        
+        return AnyView(MailView(isShowing: $isShowingMailView, result: $result))
     }
 }
 
